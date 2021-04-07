17a8e7936847f032e9907e7e2f663c1dfde1c385
Wilker Lopes <wilkerlopes257@gmail.com>
2021-04-07 16:10:42 -0400

    GameDot - change canvas id

--- public/games/GameDot/js/game.js
+++ public/games/GameDot/js/game.js
@@ -1,4 +1,4 @@
-var game = document.getElementById("forestGame");
+var game = document.getElementById("GameDot");
 var ctx = game.getContext("2d");
 
 var gravit = 0;
@@ -12,42 +12,37 @@ var limitLef = 0;
 var limitBot = 400;
 var limitRig = 700;
 
-$(document).keypress(function(e) {
-    var key = e.keyCode;
-    if (key == 32) {
-        yp -= 25;
-        gravit = 0;
-    }
+$(document).keypress(function (e) {
+  var key = e.keyCode;
+  if (key == 32) {
+    yp -= 25;
+    gravit = 0;
+  }
 });
 
-
-
-
 function draw() {
+  ctx.fillStyle = "#258936";
+  ctx.fillRect(xp, yp, 25, 25);
+  gravit += gravitSpeed;
 
+  if (yp < limitBot - 25) {
+    yp += gravit;
+  } else {
+    gravit = 0;
+  }
 
-    ctx.fillStyle = "#258936";
-    ctx.fillRect(xp, yp, 25, 25);
-    gravit += gravitSpeed;
-
-    if (yp < (limitBot - 25)) {
-        yp += gravit;
-    } else { gravit = 0 }
-
-    console.log("ttt")
-
+  console.log("ttt");
 }
 
 function clearGame() {
-    ctx.clearRect(0, 0, limitRig, limitBot);
+  ctx.clearRect(0, 0, limitRig, limitBot);
 }
 
 function updateGame() {
-    clearGame();
-    draw();
+  clearGame();
+  draw();
 
-    requestAnimationFrame(updateGame);
+  requestAnimationFrame(updateGame);
 }
-
 
 $(document).ready(updateGame());
